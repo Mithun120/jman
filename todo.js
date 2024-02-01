01bda041314fc37eddedd32bce9e39bc230fb614
MITHUN M <91070814+Mithun120@users.noreply.github.com>
2024-02-01 18:12:41 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,36 +1,46 @@
 import { useState } from 'react';
-
+let idx=0
 export default function App() {
   const [inputValue , setinputValue] = useState('');
   const [storeValue, setstoreValue] = useState([]);
   const [edit,setEdit]=useState(false);
+  const [changeValue,setchangeValue]=useState('');
   function deleteFunction(id){
     const newVal=[...storeValue]
     newVal.splice(id,1);
     setstoreValue([...newVal] )
   }
   function updateFunction(id){
+    idx=id
     setEdit(true)
   }
   function editFunction(){
+    let tempList=[...storeValue]
+    let tempobj={inputValue:changeValue}
+    tempList.splice(idx,1,tempobj)
+    setstoreValue(tempList)
     setEdit(false)
   }
   return (
     <>
       <h1>Todo list</h1>
+      {!edit&&
       <form>
+
       <input
         
         onChange={e => setinputValue(e.target.value)}
       />
-    {edit && <><input type='text'></input> <button onClick={editFunction()}>save</button></>}
+    </form>}
+    
+    {edit && <form><input type='text' onChange={e=> setchangeValue(e.target.value)}></input> <button onClick={()=>editFunction()}>save</button>  </form>}
       <button type="reset" onClick={() => {
         setstoreValue([
           ...storeValue,
           { inputValue: inputValue }
         ]);
       }}>Add</button>
-      </form>
+    
       <ul>
         {storeValue.map((a,id) => (<>
           <li key={id}>{a.inputValue}</li>
@@ -42,4 +52,5 @@ export default function App() {
       
     </>
   );
-}
+}  
+                  
